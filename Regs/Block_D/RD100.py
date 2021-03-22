46cb95aa66726c955787ee790f2f0b8fefec6e22
Bernardo Bello <bernardo.bello95@hotmail.com>
2021-03-22 19:53:36 -0300

    Core & Regs > Block D partially implemented
    Signed-off-by: Bernardo Bello <bernardo.bello95@hotmail.com>

---
+++ Regs/Block_D/RD100.py
@@ -0,0 +1,31 @@
+from ..IReg import IReg
+
+
+class RD100(IReg):
+
+    def __init__(self):
+        self._header = ['REG',
+                        'IND_OPER',
+                        'IND_EMIT',
+                        'COD_PART',
+                        'COD_MOD',
+                        'COD_SIT',
+                        'SER',
+                        'SUB',
+                        'NUM_DOC',
+                        'CHV_CTE',
+                        'DT_DOC',
+                        'DT_A_P',
+                        'TP_CT-e',
+                        'CHV_CTE_REF',
+                        'VL_DOC',
+                        'VL_DESC',
+                        'IND_FRT',
+                        'VL_SERV',
+                        'VL_BC_ICMS',
+                        'VL_ICMS',
+                        'VL_NT',
+                        'COD_INF',
+                        'COD_CTA',
+                        'COD_MUN_ORIG',
+                        'COD_MUN_DEST']
---
+++ Regs/Block_D/__init__.py
@@ -0,0 +1,2 @@
+from .RD001 import RD001
+from .RD100 import RD100
