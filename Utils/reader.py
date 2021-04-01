7c741110ce1ee81a54bfa3c78e5da7fe89f4a2e2
Bernardo Bello <bernardo.bello95@hotmail.com>
2021-04-01 09:59:11 -0300

    Reader strip digital signature
    Signed-off-by: Bernardo Bello <bernardo.bello95@hotmail.com>

--- Utils/reader.py
+++ Utils/reader.py
@@ -28,6 +28,41 @@ class Reader:
             with open(file, 'r', encoding='mbcs') as sped:
                 file_list = sped.read().splitlines()
 
+            if not self.isSigned(file_list):
+                file_list = self.stripSignature(file_list)
+
             self._file_dict[file] = file_list
 
         return self._file_dict
+
+    @staticmethod
+    def isSigned(file):
+        """
+        Checks if the file has a signature embeded or not.
+
+        :param file: Sped Fiscal
+        :type file: list
+        :return: True or False statemente
+        :rtype: boolean
+        """
+
+        if '|9999|' in file[-1]:
+            return True
+        else:
+            return False
+
+    @staticmethod
+    def stripSignature(file):
+        """
+        Strips out all lines from the digital signature.
+
+        :param file: Sped Fiscal
+        :type file: list
+        :return: Stripped Sped Fiscal
+        :rtype: list
+        """
+
+        while '|9999|' not in file[-1]:
+            file.pop(-1)
+
+        return file
