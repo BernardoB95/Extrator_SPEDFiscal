24916fc19e22b8af249432342361187e006a6d79
Bernardo Bello <bernardo.bello95@hotmail.com>
2021-04-22 19:29:08 -0300

    Utils > Reg Binder Updated
    Signed-off-by: Bernardo Bello <bernardo.bello95@hotmail.com>

--- Utils/reg_binder.py
+++ Utils/reg_binder.py
@@ -2,63 +2,25 @@
 from Regs import NullReg
 from itertools import count
 
-def bind_id(binding_list, reg, index):
-    """
-    Function that will perform the REGs binding in order to identify the hierarchy.
-    Object reg is modified inside the function.
-    :param binding_list: list of reg objects
-    :type binding_list: list
-    :param reg: REG identifier
-    :type reg: str
-    :param index: current index of the reg to work as a pointer
-    :type index: int
-    :return: index of the super(pythonic) or father hierarchy reg
-    :rtype: int
-    """
+class Binder:
 
-    if reg.hierarchy == '0':
-        reg.id_super = None
-    elif isinstance(reg, NullReg):
-        pass
-    else:
+    def __init__(self):
+        self._memory_binding_dict = {}
 
-        # pointer = index
-        # while pointer >= 0:
-        #
-        #     if isinstance(binding_list[pointer], NullReg):
-        #         pointer -= 1
-        #         continue
-        #
-        #     pointer_h = binding_list[pointer].hierarchy
-        #
-        #     if pointer_h < reg.hierarchy:
-        #         reg.id_super = binding_list[pointer].id
-        #         break
-        #     pointer -= 1
+    def bind_id(self, reg):
 
-        # for pointer in range(index, -1, -1):
-        #
-        #     if isinstance(binding_list[pointer], NullReg):
-        #         continue
-        #
-        #     pointer_h = binding_list[pointer].hierarchy
-        #
-        #     if pointer_h < reg.hierarchy:
-        #         reg.id_super = binding_list[pointer].id
-        #         break
+        try:
+            if reg.hierarchy == "0":
+                reg.id_super = None
+                self._memory_binding_dict[reg.hierarchy] = reg
 
-        counter = count(index, -1)
+            elif isinstance(reg, NullReg):
+                raise AttributeError
 
-        for pointer in counter:
+            else:
+                hierarchy_super = str(int(reg.hierarchy) - 1)
+                reg.id_super = self._memory_binding_dict[hierarchy_super].id
+                self._memory_binding_dict[reg.hierarchy] = reg
 
-            if pointer == -1:
-                break
-
-            if isinstance(binding_list[pointer], NullReg):
-                continue
-
-            pointer_h = binding_list[pointer].hierarchy
-
-            if pointer_h < reg.hierarchy:
-                reg.id_super = binding_list[pointer].id
-                break
+        except AttributeError:
+            pass
